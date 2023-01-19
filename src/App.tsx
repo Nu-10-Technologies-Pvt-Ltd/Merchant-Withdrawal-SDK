a54a99a12986a0b53c15932a2a32495edb9155b0
sushantgawai <sushantg@nu10.co>
2023-01-19 13:18:34 +0530

    removed paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
 import CryptoTnx from "./pages/CryptoTnx/CryptoTnx";
-import Login from "./pages/login/login";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
         <Route path="/crypto-transaction" element={<CryptoTnx />} />
-        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
