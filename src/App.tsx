5ce80c9b27a3bb244e20196f17ff4f64acad388a
Animesh <animesha@nu10.co>
2023-01-19 20:19:20 +0530

    update: approval page added

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Approval from "./pages/Approval/Approval";
 
 import CryptoTnx from "./pages/CryptoTnx/CryptoTnx";
 
-
 import Login from "./pages/login/login";
+import TwoFa from "./pages/twofa/TwoFa";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-
-        <Route path="/crypto-transaction" element={<CryptoTnx />} />
-
         <Route path="/" element={<Login />} />
-        {/* <Route path="/about" element={<About />} /> */}
+        <Route path="/crypto-transaction" element={<CryptoTnx />} />
+        <Route path="/approval" element={<Approval />} />
+        <Route path="/twofa" element={<TwoFa />} />
 
+        {/* <Route path="/about" element={<About />} /> */}
       </Routes>
     </BrowserRouter>
   );
