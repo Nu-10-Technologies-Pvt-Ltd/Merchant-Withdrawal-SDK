4532d8f5eb2862ec27d2c0464fd0ca0cb60486c6
Animesh <animesha@nu10.co>
2023-01-30 15:16:01 +0530

    update: endpoints changed

--- src/api/http-common.ts
+++ src/api/http-common.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default axios.create({
   baseURL:
-    "http://internal-nivapay-main-api-168396609.us-east-1.elb.amazonaws.com:5002",
+    "http://nivapay-main-api-lb2-77118926.us-east-1.elb.amazonaws.com:5002",
   headers: {
     // "Content-type": "application/json",
   },
